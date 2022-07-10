ef5d8244d9ec895c66e68b16f2dc7e821334ca56
Felipe Libório <felipeliborio@outlook.com>
2022-07-09 21:48:06 -0300

    Update index.js
    Adding a benchmark that uses a set instead of a manually generated map as a more idiomatic and way faster approach. Just to make clear that neither the array or manually created map aproaches would be faster in JS.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function generateArrayPairs() 
-{
+const INSTANCE_COUNT = 100;
+const INSTANCE_SIZE = 1000000;
+
+function generateArrayPairs() {
   let arrayPairs = [];
-  for (let i = 0; i < 100; ++i) 
-  {
+  for (let i = 0; i < INSTANCE_COUNT; ++i) {
     let a1 = [];
     let a2 = [];
 
-    for(let j = 0; j < 1000000; ++j) 
-    {
-      let n1 = ~~(10000000*Math.random());
-      let n2 = ~~(10000000*Math.random());
+    for (let j = 0; j < INSTANCE_SIZE; ++j) {
+      let n1 = ~~(10 * INSTANCE_SIZE * Math.random());
+      let n2 = ~~(10 * INSTANCE_SIZE * Math.random());
       a1.push(n1);
       a2.push(n2);
     }
@@ -19,10 +19,12 @@ function generateArrayPairs()
   return arrayPairs;
 }
 
-function binarySearch(arr, searchElement){
-  let left = 0, right = arr.length - 1, mid;
+function binarySearch(arr, searchElement) {
+  let left = 0,
+    right = arr.length - 1,
+    mid;
   while (right > left) {
-    mid = ~~((left + right + 1) / 2);
+    mid = (left + right + 1) >> 1;
     if (arr[mid] === searchElement) {
       return mid;
     }
@@ -35,9 +37,8 @@ function binarySearch(arr, searchElement){
   return -1;
 }
 
-function findIntersectionBS(array1, array2)
-{
-  array2.sort((a, b) => a-b);
+function findIntersectionBS(array1, array2) {
+  array2.sort((a, b) => a - b);
   let found = 0;
   for (let i = 0; i < array1.length; ++i) {
     binarySearch(array2, array1[i]) > -1 && (array1[found++] = array1[i]);
@@ -45,13 +46,21 @@ function findIntersectionBS(array1, array2)
   array1.length = found;
 }
 
-function findIntersectionHash(array1, array2) 
-{
+function findIntersectionHash(array1, array2) {
   let array2Map = {};
-  array2.map(e => array2Map[e] = true);
+  array2.map((e) => (array2Map[e] = true));
   let found = 0;
   for (let i = 0; i < array1.length; ++i) {
     array2Map[array1[i]] && (array1[found++] = array1[i]);
+  }
+  array1.length = found;
+}
+
+function findIntersectionSet(array1, array2) {
+  let array2Set = new Set(array2);
+  let found = 0;
+  for (let i = 0; i < array1.length; ++i) {
+    array2Set.has(array1[i]) && (array1[found++] = array1[i]);
   }
   array1.length = found;
 }
@@ -63,18 +72,27 @@ arrayPairs = generateArrayPairs();
 console.timeEnd("Init arrays for hash");
 
 console.time("Find intersections hash");
-arrayPairs.map(e => {
+arrayPairs.map((e) => {
   findIntersectionHash(...e);
 });
 console.timeEnd("Find intersections hash");
-
 
 console.time("Init arrays for BS");
 arrayPairs = generateArrayPairs();
 console.timeEnd("Init arrays for BS");
 
 console.time("Find intersections BS");
-arrayPairs.map(e => {
+arrayPairs.map((e) => {
   findIntersectionBS(...e);
 });
 console.timeEnd("Find intersections BS");
+
+console.time("Init arrays for set");
+arrayPairs = generateArrayPairs();
+console.timeEnd("Init arrays for set");
+
+console.time("Find intersections set");
+arrayPairs.map((e) => {
+  findIntersectionSet(...e);
+});
+console.timeEnd("Find intersections set");
